0edfca6f347a87bf72e6c310b985328b8d1c896e
Divya <deevyapanthi03@gmail.com>
2020-10-22 06:19:25 +0545

    User Profile and Picture, Update User Profile

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+from django.views.generic import ListView
 
 
 def home(request):
@@ -8,6 +9,10 @@ def home(request):
         'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
+
+class PostListView(ListView):
+    model = Post
 
 
 def about(request):
